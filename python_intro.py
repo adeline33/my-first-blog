86efbb3036b19a3a648b523a529b1f5a03cd9719
Adeline <adeline.labbe.bouquet@gmail.com>
2016-04-09 15:37:02 +0200

    django girls 1er commit

---
+++ python_intro.py
@@ -0,0 +1,12 @@
+def hi ():
+	print('hi there!')
+	print('how are you?')
+hi()  
+def hi (name):
+	if name=='Adeline'
+	print hi ('Adeline')
+elif name == 'Sonja':
+	print hi ('Sonja')
+else:
+	print hi ('anonymous!')
+
